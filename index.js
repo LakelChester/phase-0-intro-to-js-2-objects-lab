dd43d1830fc9c30eddf14601e07ebea5ff616fe1
LakelChester <lakel@chesterassociate.com>
2022-06-19 19:15:36 -0700

    commit tree be688519ae455827893b18383c06ed6d3ae14f89

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Johnny',
+    streetAddress : "21 Jump St",
+}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    
+    const newEmployee = {...employee};
+
+    newEmployee[key] = value
+
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee
+
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+ 
+    const newEmployeeKey = Object.assign({}, employee)
+    
+    delete newEmployeeKey[key];
+    
+    return newEmployeeKey
+}
+
+function destructivelyDeleteFromEmployeeByKey(newEmployee, key){
+
+    delete newEmployee[key]
+
+    return newEmployee
+
+
+}
